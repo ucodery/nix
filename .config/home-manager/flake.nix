70763c6eda8c1ec10804c9a13ff432e8308628e9
Jeremiah Paige <ucodery@gmail.com>
2026-09-09 09:20:55 -0700

    leave darwin/x64 behind

--- .config/home-manager/flake.nix
+++ .config/home-manager/flake.nix
@@ -11,19 +11,7 @@
 
   outputs = { nixpkgs, home-manager, ... }:
     {
-      defaultPackage.x86_64-darwin = home-manager.defaultPackage.x86_64-darwin;
-      defaultPackage.aarch64-darwin = home-manager.defaultPackage.aarch64-darwin;
-      formatter.x86_64-darwin = nixpkgs.legacyPackages.x86_64-darwin.nixpkgs-fmt;
-      formatter.aarch64-darwin = nixpkgs.legacyPackages.aarch64-darwin.nixpkgs-fmt;
-      homeConfigurations."jeremyp@AppleState" = home-manager.lib.homeManagerConfiguration {
-        system = "x86_64-darwin";
-        pkgs = nixpkgs.legacyPackages.x86_64-darwin;
-
-        # config goes here
-        modules = [ ./home.nix ];
-      };
-      homeConfigurations."jeremyp@ActivePro" = home-manager.lib.homeManagerConfiguration {
-        system = "aarch64-darwin";
+      homeConfigurations."jeremyp" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.aarch64-darwin;
 
         # config goes here
