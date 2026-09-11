c733ac8a2a04611bf2c6f01103d8831f46125b03
Jeremiah Paige <ucodery@gmail.com>
2026-09-11 15:39:53 -0700

    nvim: floating windows follow transparency

--- .config/nvim/lua/me/transparent.lua
+++ .config/nvim/lua/me/transparent.lua
@@ -1,1 +1,3 @@
--- setup not required
+-- floating windows (terminal popup, leader help) draw with their own groups,
+-- which transparent.nvim does not clear by default
+vim.g.transparent_groups = vim.list_extend(vim.g.transparent_groups or {}, { 'NormalFloat', 'FloatBorder', 'FloatTitle' })
