ff6ca783a1b7713bc213957e6db99ee1abb5a39d
Jeremiah Paige <ucodery@gmail.com>
2026-09-11 15:39:53 -0700

    home.nix: drop nodejs and the template comments

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -39,7 +39,6 @@
     # Editor (neovim itself is programs.neovim below)
     pkgs.black
     pkgs.lua-language-server
-    pkgs.nodejs
     pkgs.prettier
     pkgs.pyright
     pkgs.rust-analyzer
@@ -50,19 +49,6 @@
     pkgs.asciinema
     pkgs.ffmpeg
     pkgs.figlet
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
   ];
 
   home.file = {
@@ -80,34 +66,8 @@
     ".ssh/config".source = ../../.ssh/config;
     "bin/edit".source = ../../bin/edit;
     "bin/gen".source = ../../bin/gen;
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/jeremyp/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
     EDITOR = "edit here block --";
     VISUAL = "${config.home.sessionVariables.EDITOR}";
